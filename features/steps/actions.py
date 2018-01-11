b6611c3f02bd4c9b77990115e9753d0a75590952
TonyCross <starovoitov.anton@gmail.com>
2018-01-11 23:01:12 +0500

    Реализация дальнейших функций

--- features/steps/actions.py
+++ features/steps/actions.py
@@ -1,33 +1,23 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 
 use_step_matcher("parse")
 
 
 @step('Поставили галочку "{text}"')
 def step_impl(context, text):
-    """
-    :type context: behave.runner.Context
-    """
     pass
 
 
 @given('Зашли на сайт "{url}"')
 def step_impl(context, url):
-    """
-    :type context: behave.runner.Context
-    """
     context.driver = webdriver.Chrome()
     context.driver.get(url)
 
 
 @then("Проверили, что появилось поле поиска")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
     try:
         context.driver.find_element_by_id("lst-ib")
         print('Поле поиска найдено!')
@@ -37,9 +27,6 @@ def step_impl(context):
 
 @step('В поле "{field_name}" ввели "{text}"')
 def step_impl(context, field_name, text):
-    """
-    :type context: behave.runner.Context
-    """
     if field_name.lower() == 'поиск':
         element = context.driver.find_element_by_id("lst-ib")
         element.send_keys(text)
@@ -47,9 +34,6 @@ def step_impl(context, field_name, text):
 
 @then('Нажали кнопку "{button_text}"')
 def step_impl(context, button_text):
-    """
-    :type context: behave.runner.Context
-    """
     if button_text.lower() == 'enter':
         element = context.driver.find_element_by_id("lst-ib")
         element.send_keys(Keys.RETURN)
@@ -57,9 +41,6 @@ def step_impl(context, button_text):
 
 @then('Проверили, что открылась страница "{title}"')
 def step_impl(context, title):
-    """
-    :type context: behave.runner.Context
-    """
     title = context.driver.title
     print(title)
     assert 'Центральный банк' in title
@@ -67,24 +48,15 @@ def step_impl(context, title):
 
 @then('Нажали на ссылку "{link_text}"')
 def step_impl(context, link_text):
-    """
-    :type context: behave.runner.Context
-    """
-    element = context.driver.find_element_by_xpath("//a[contains(text(), 'Центральный банк Российской Федерации')]")
+    element = context.driver.find_element_by_xpath("//a[contains(text(), '{}')]".format(link_text))
     element.click()
 
 
 @step("Сделали скриншот")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
     pass
 
 
 @then('Отправили скриншот на почту "{email}"')
 def step_impl(context, email):
-    """
-    :type context: behave.runner.Context
-    """
     pass
